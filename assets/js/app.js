11789681f3acfdfa17ba4a429cdf59a03460862e
soniabergamini <sounia96@outlook.it>
2023-05-10 14:44:34 +0200

    feat(main): add todo list elements on page

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,15 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
+            todoList: [
+                { text: 'Go to the market',         done: true }, 
+                { text: 'Have launch with Jack',    done: false }, 
+                { text: 'Send emails',              done: false }, 
+                { text: 'Learn VueJS',              done: true },
+                { text: 'Take a walk with Dea🦮',   done: false }
+            ],
+            mainClasses: "dFlex alignCenter justyCenter",
+            liClasses: "dFlex gap2em justySpaceBtw",
         }
     }
 }).mount('#app')
